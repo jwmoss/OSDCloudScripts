6d2e8d0b97bda8275da254a7f02ee6ef6c564943
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-06-23 10:10:04 -0400

    Add step to load drivers

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -27,8 +27,6 @@ $Params = @{
     Firmware = $false
 }
 
-$Global:OSDCloud.OSDCloudUnattend = $true
-
 Start-OSDCloud @Params
 
 #Install-WindowsUpdate -UpdateType Driver -AcceptAll -IgnoreReboot
@@ -38,12 +36,36 @@ Start-OSDCloud @Params
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
 $SetupCompleteCMD = @'
 powershell.exe -Command Set-ExecutionPolicy Unrestricted -Force
-Invoke-OSDSpecialize
 powershell.exe -Command "& {IEX (IRM 'https://raw.githubusercontent.com/jwmoss/ronin_puppet/win11/provisioners/windows/OSDCloud/bootstrap.ps1')}"
 '@
 $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
 
 & "X:\OSDCloud\Config\Scripts\Shutdown\local_keyvault.ps1"
+
+## Setup driver path
+$PathPanther = 'C:\Windows\Panther'
+if (-NOT (Test-Path $PathPanther)) {
+    New-Item -Path $PathPanther -ItemType Directory -Force | Out-Null
+}
+
+$UnattendDrivers = @'
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="offlineServicing">
+        <component name="Microsoft-Windows-PnpCustomizationsNonWinPE" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <DriverPaths>
+                <PathAndCredentials wcm:keyValue="1" wcm:action="add">
+                    <Path>C:\Drivers</Path>
+                </PathAndCredentials>
+            </DriverPaths>
+        </component>
+    </settings>
+</unattend>
+'@
+
+$UnattendPath = Join-Path $PathPanther 'Unattend.xml'
+Write-Verbose -Verbose "Setting Driver $UnattendPath"
+$UnattendDrivers | Out-File -FilePath $UnattendPath -Encoding utf8
 
 Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
